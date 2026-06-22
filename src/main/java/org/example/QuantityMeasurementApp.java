31bc90c82c8c5d6eb2b59e6eb07fb1ac0231f727
E Bharati Patra <bharati.patra@bridgelabz.com>
2026-06-22 11:27:39 +0530

    UC16: Database Integration with JDBC for Quantity Measurement Persistence

--- src/main/java/org/example/QuantityMeasurementApp.java
+++ src/main/java/org/example/QuantityMeasurementApp.java
@@ -2,7 +2,8 @@ package org.example;
 import org.example.controller.QuantityMeasurementController;
 import org.example.dto.QuantityDTO;
 import org.example.repository.IQuantityMeasurementRepository;
-import org.example.repository.QuantityMeasurementCacheRepository;
+import org.example.repository.QuantityMeasurementDatabaseRepository;
+import org.example.util.DatabaseInitializer;
 import org.example.service.IQuantityMeasurementService;
 import org.example.service.QuantityMeasurementServiceImpl;
 
@@ -10,9 +11,11 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        // Factory + DI
+        //  Initialize DB
+        DatabaseInitializer.init();
+
         IQuantityMeasurementRepository repo =
-                QuantityMeasurementCacheRepository.getInstance();
+                new QuantityMeasurementDatabaseRepository();
 
         IQuantityMeasurementService service =
                 new QuantityMeasurementServiceImpl(repo);
@@ -20,20 +23,9 @@ public class QuantityMeasurementApp {
         QuantityMeasurementController controller =
                 new QuantityMeasurementController(service);
 
-        // Example Usage
         controller.performComparison(
                 new QuantityDTO(1, "FEET", "LENGTH"),
                 new QuantityDTO(12, "INCHES", "LENGTH")
-        );
-
-        controller.performConversion(
-                new QuantityDTO(100, "CELSIUS", "TEMPERATURE"),
-                "FAHRENHEIT"
-        );
-
-        controller.performAddition(
-                new QuantityDTO(1, "KILOGRAM", "WEIGHT"),
-                new QuantityDTO(1000, "GRAM", "WEIGHT")
         );
     }
 }
